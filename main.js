a4003710c07764e44a6ed44ccff8eb91ad6f3219
Lora <lora@seemelissa.com>
2018-02-20 22:43:02 +0200

    Add animationn

--- main.js
+++ main.js
@@ -1,30 +1,10 @@
-// $(document).ready(function() {
-//     $('#card').on('click', function () {
-//         $(this).css("width", "800");
-//     })
-// });
-//
-// $('#card').on('click', function () {
-//     $(this).css('background-color', 'blue');
-// });
-
 function animation() {
-
-// Your code goes here!
-    $('#card').on('click', 'td', function () {
-        var color= "black";
-        $(this).css('background-color', color);
+    $('#card').on('click', function () {
+        $(this).addClass("animate-card");
     });
-
-    // opacity.on('click', 'td', function(){
-    //     var color= $('#colorPicker').val();
-    //     var opacity = $('#opacity').val();
-    //     $(this).css({ opacity: opacity })
-    // });
 
 }
 
-
-$("#card").on('click',function (event) {
+$("#card").on('click',function () {
     animation();
 });
